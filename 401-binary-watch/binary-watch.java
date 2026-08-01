cf96fed9ef8f944ca85c832aa19061b7ae110c27
BASKARAN K <baskaranamaran@gmail.com>
2026-08-01 11:05:42 +0530

    Time: 1 ms (99.53%) | Memory: 43.2 MB (99.25%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -4,7 +4,17 @@ class Solution {
         for(int i=0;i<12;i++){
             for(int j=0;j<60;j++){
                 if(Integer.bitCount(i)+Integer.bitCount(j)==turnedOn){
-                    list.add(String.format("%d:%02d",i,j));
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(i);
+                    sb.append(":");
+                    if(j<10){
+                        sb.append(0);
+                        sb.append(j);
+                    }
+                    else{
+                        sb.append(j);
+                    }
+                    list.add(sb.toString());
                 }
             }
         }
