b74274ed8d7affdf5b19be5263fd56e6f0cbe9c9
BASKARAN K <baskaranamaran@gmail.com>
2026-08-01 11:59:49 +0530

    Time: 10 ms (8.44%) | Memory: 48.1 MB (98.03%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr = new int[n+1];
+        for(int i=0;i<=n;i++){
+            int count = 0;
+            int a = i;
+            while(a != 0){
+                int rem = a % 2;
+                if(rem==1)count++;
+                a /= 2;
+            }
+            arr[i] = count;
+        }
+        return arr;
+    }
+}
