8b8e57826f322feef7f789f431ed883bd397f5d0
BASKARAN K <baskaranamaran@gmail.com>
2026-08-08 14:50:15 +0530

    Time: 599 ms (8.84%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,1 +1,1 @@
-select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on p.personId = a.PersonId;
+select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on a.personId = p.PersonId;
