78204b23d625f72b71a863cf1ffd7bbc03088ea4
BASKARAN K <baskaranamaran@gmail.com>
2026-08-18 14:49:13 +0530

    Time: 1 ms (99.98%) | Memory: 42.4 MB (91.39%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,14 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        if(x == 1534236469 || x == 2147483647 || x == -2147483648 || x == -1563847412 || x == 1563847412
-        || x == 1147483648 || x == 1137464807  || x == 1235466808 || x == 1221567417  || x == -1534236469)return 0;
-        int a = 0;
+        
+        long a = 0;
         while(x != 0){
             int rem = x % 10;
             a = a * 10;
             a += rem;
             x /= 10;
         }
-        return a;
+        if(a > Integer.MAX_VALUE || a < Integer.MIN_VALUE)return 0;
+        return (int)a;
     }
 }
