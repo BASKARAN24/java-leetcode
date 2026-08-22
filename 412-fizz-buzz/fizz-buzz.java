01675a6bf8ce2f1f80d6a3c2ca4a41dc3b8f8a22
BASKARAN K <baskaranamaran@gmail.com>
2026-08-22 14:31:29 +0530

    Time: 2 ms (13.67%) | Memory: 46.9 MB (45.85%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int i = 1;
+        List<String> list = new ArrayList<>();
+        while(i <= n){
+            if((i%3 == 0) && (i%5 == 0))list.add("FizzBuzz");
+            else if(i%3 == 0)list.add("Fizz");
+            else if(i%5 == 0)list.add("Buzz");
+            else list.add(i+"");
+            i++;
+        }
+        return list;
+    }
+}
